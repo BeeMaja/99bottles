218df48d8479aa4108343ae4de462b7c160f25ff
BeeMaja <herz_stefan@yahoo.de>
2017-12-06 14:44:08 +0100

    fu typo
    gnnn... typo

--- 99bottles.py
+++ 99bottles.py
@@ -5,9 +5,9 @@ while i:
     if i > 1:
         print("{0} bottles of beer on the wall, {0} bottles of beer.".format(i))
         i -= 1
-        print("Take one down an pass it around, {0} bottles of beer on the wall\n".format(i))
+        print("Take one down an pass it around, {0} bottles of beer on the wall.\n".format(i))
     else:
         print("{0} bottle of beer on the wall, {0} bottle of beer.".format(i))
         i -= 1
         print("Take one down and pass it around, no more bottles of beer on the wall.\n")
-print("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store by some more, 99 bottles of beer on the wall")
+print("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store by some more, 99 bottles of beer on the wall.")
